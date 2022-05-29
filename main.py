924e3869be15d72df7f150693afef093f0c451c0
Thomas Sjaastad <thomas.sjaastad@gmail.com>
2022-05-29 20:02:52 +0200

    add instrument grid

--- main.py
+++ main.py
@@ -18,11 +18,35 @@ label_font = pygame.font.Font('freesansbold.ttf', 32)
 fps = 60
 timer = pygame.time.Clock()
 
+
+def draw_grid():
+    left_box = pygame.draw.rect(screen, gray, [0, 0, 200, HEIGHT - 200], 5)
+    botton_box = pygame.draw.rect(
+        screen, gray, [0, HEIGHT - 200, WIDTH, 200], 5)
+    boxes = []
+    color = [gray, white, gray]
+    hi_hat_text = label_font.render('Hi Hat', True, white)
+    screen.blit(hi_hat_text, (30, 30))
+    kick_text = label_font.render('Kick', True, white)
+    screen.blit(kick_text, (30, 130))
+    snare_text = label_font.render('Snare', True, white)
+    screen.blit(snare_text, (30, 230))
+    crash_text = label_font.render('Crash', True, white)
+    screen.blit(crash_text, (30, 330))
+    floor_tom_text = label_font.render('Floor tom', True, white)
+    screen.blit(floor_tom_text, (30, 430))
+    clap_text = label_font.render('Clap', True, white)
+    screen.blit(clap_text, (30, 530))
+    for i in range(6):
+        pygame.draw.line(
+            screen, gray, (0, (i*100) + 100), (200, (i*100) + 100), 5)
+
+
 run = True
 while run:
     timer.tick(fps)
     screen.fill(black)
-
+    draw_grid()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
